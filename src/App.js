a6983e690b5d4e557de7d4de8bc8a43648f563d2
RonaldVilorio <ronvilorio@gmail.com>
2020-07-22 12:28:16 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={
+            "https://www.innovateli.com/wp/wp-content/uploads/2020/02/Nebula-Academy-logo-2.png"
+          }
+          className="App-logo"
+          alt="logo"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
